8aa403fe6f5681585ccae0b96a7d640ba9fd94c1
Lenovo Y700-15ISK <aleksander360@gmail.com>
2018-12-01 14:35:35 +0100

    [add] monkey

---
+++ src/main/java/pl/alek/my_git/Monkey.java
@@ -0,0 +1,11 @@
+package pl.alek.my_git;
+
+public class Monkey extends Animal{
+
+    String size;
+
+    public Monkey(String name, String size) {
+        super(name);
+        this.size = size;
+    }
+}
