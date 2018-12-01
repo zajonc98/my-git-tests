9e2de0683e2ad3a689ea82f28b3bd7744442fb3a
Lenovo Y700-15ISK <aleksander360@gmail.com>
2018-12-01 11:39:38 +0100

    [add] - class samochod

---
+++ src/main/java/pl/alek/my_git/Samochód.java
@@ -0,0 +1,12 @@
+package pl.alek.my_git;
+
+public class Samochód {
+    String marka;
+    String model;
+
+
+    public Samochód(String marka, String model) {
+        this.marka = marka;
+        this.model = model;
+    }
+}
