677cfd00998e6e2b12460dc9dc43f57c574f93e7
Lenovo Y700-15ISK <aleksander360@gmail.com>
2018-12-01 11:44:06 +0100

    [change] - in class samochod

--- src/main/java/pl/alek/my_git/Samochód.java
+++ src/main/java/pl/alek/my_git/Samochód.java
@@ -9,4 +9,28 @@ public class Samochód {
         this.marka = marka;
         this.model = model;
     }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "Samochód{" +
+                "marka='" + marka + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
 }
